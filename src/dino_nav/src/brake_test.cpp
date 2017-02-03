9e2f777ecbb9783dfaaae19126832abd3013b472
Francesco Gatti <gattifrancesco@hotmail.it>
2017-02-03 13:30:10 +0100

    brake test example

--- src/dino_nav/src/brake_test.cpp
+++ src/dino_nav/src/brake_test.cpp
@@ -8,9 +8,48 @@ struct vquad_t {
     float x, y, l;
 };
 
-enum state_e { PREPARE, START, BREAK };
+enum state_e { PREPARE, START, BRAKE, END };
+
+
+const int MAX_TESTS = 8;
+const int MAX_VELS = 1024;
+struct test_t {
+    float throttle, brake;
+    float vels[MAX_VELS], vels_n;
+    float speed_reached, start, brake_start, brake_end;
+} tests[MAX_TESTS];
+int test_num;
 
 ros::Publisher drive_pub;
+
+void print_tests(float_point_t pos, int cur_test) {
+
+    for(int i=0; i<cur_test; i++) {
+        test_t *t = &tests[i];
+        float offs = 40*i;
+
+        viz_text(pos.x, pos.y + offs, 12, VIEW_COLOR, 
+            "TEST %d, throttle: %f  brake: %f", i, t->throttle, t->brake);
+        viz_text(pos.x, pos.y + offs +10, 12, VIEW_COLOR, 
+            "dists: start %f, brake start: %f  brake end: %f", t->start, t->brake_start, t->brake_end);
+         viz_text(pos.x, pos.y + offs +20, 12, VIEW_COLOR, 
+            "max speed %f, acc dist %f, brake dist %f", 
+            t->speed_reached, t->start - t->brake_start, t->brake_start - t->brake_end);           
+    }
+}
+
+void print_test_text(int id) {
+    test_t *t = &tests[id];
+
+    printf("\n");
+    printf("-------- TEST %d --------\n", id);
+    printf("throttle: %f  brake: %f\n", t->throttle, t->brake);
+    printf("dists: start %f, brake start: %f  brake end: %f\n", 
+        t->start, t->brake_start, t->brake_end);
+    printf("max speed %f, acc dist %f, brake dist %f\n", 
+        t->speed_reached, t->start - t->brake_start, t->brake_start - t->brake_end); 
+    printf("-------------------------\n\n");
+}
 
 float mean_ray(std::vector<float> v, int idx, int l) {
     float sum = 0;
@@ -26,7 +65,6 @@ void run_test(float &throttle, float &steer, float wall_dist, vquad_t &view) {
     const float min_dist = 3;
 
     static state_e state = PREPARE;
-    static int test = 0; 
 
     static float old_dist = wall_dist;
     static float speed = 0;
@@ -40,35 +78,65 @@ void run_test(float &throttle, float &steer, float wall_dist, vquad_t &view) {
     }
     n++;
 
+    static int current_test = 0;
+    if(n == 1)
+        printf("test %d PREPARE\n", current_test);
+
+    test_t *test = &tests[current_test];
+
     switch(state) {
 
     case PREPARE:
-        viz_text(view.x + view.l + 20, view.y +140, 15, VIEW_COLOR, "PREPARE");
+        viz_text(view.x + view.l + 20, view.y +140, 15, VIEW_COLOR, "test %d status: PREPARE");
         if(wall_dist < start_dist) {
-            throttle = -20;
+            throttle = 0;
             steer = -steer;
         } else {
             state = START;
+            tests->start = wall_dist;
+            printf("test %d START\n", current_test);
         }
         break;
 
     case START:
-        viz_text(view.x + view.l + 20, view.y +140, 15, VIEW_COLOR, "PREPARE -> START");
-        if(wall_dist > min_dist)
-            throttle = 50;
-        else
-            state = BREAK;
+        viz_text(view.x + view.l + 20, view.y +140, 15, VIEW_COLOR, "test %d status: START");
+        if(wall_dist > min_dist) {
+            throttle = test->throttle;
+        } else {
+            state = BRAKE;
+            test->brake_start = wall_dist;
+            test->speed_reached = speed;
+            printf("test %d BRAKE\n", current_test);
+        }
         break;
 
-    case BREAK:
-        viz_text(view.x + view.l + 20, view.y +140, 15, VIEW_COLOR, "PREPARE -> START -> BREAK");
-        if(speed > 0)
-            throttle = -100;
-        else
-            state = PREPARE;
+    case BRAKE:
+        viz_text(view.x + view.l + 20, view.y +140, 15, VIEW_COLOR, "test %d status: BRAKE");
+        if(speed > 0.001 || speed < -0.001) {
+            throttle = test->brake;
+        } else {
+            test->brake_end = wall_dist;
+            print_test_text(current_test);
+            current_test++;
+            if(current_test < test_num) {
+                state = PREPARE;
+                printf("test %d PREPARE\n", current_test);
+            } else {
+                state = END;
+                printf("ALL TEST ENDED\n");
+            }
+        }
+        break;
+    
+    case END:
+        viz_text(view.x + view.l + 20, view.y +140, 15, VIEW_COLOR, "ALL TESTS EXECUTED");
         break;
     }
 
+    float_point_t test_p;
+    test_p.x = view.x + view.l + 20;
+    test_p.y = view.y + 180;
+    print_tests(test_p, current_test);
 }
 
 void laser_recv(const sensor_msgs::LaserScan::ConstPtr& msg) {
@@ -77,7 +145,7 @@ void laser_recv(const sensor_msgs::LaserScan::ConstPtr& msg) {
     vquad_t view;
     view.x = 10;
     view.y = 10;
-    view.l = 400;
+    view.l = 200;
 
     int size = msg->ranges.size();
     float orig_front_ray = mean_ray(msg->ranges, size/2, 2);
@@ -105,6 +173,8 @@ void laser_recv(const sensor_msgs::LaserScan::ConstPtr& msg) {
     float b = left_ray;    
     float d = front_ray;
     float beta = asin( (b*sin(alpha)) / sqrt(b*b + d*d - 2*b*d*cos(alpha)));
+    if(beta != beta)
+        beta = 0;
     if(b < a)
         beta = M_PI/2 - (beta - M_PI/2);
 
@@ -147,6 +217,18 @@ void laser_recv(const sensor_msgs::LaserScan::ConstPtr& msg) {
 }
 
 
+void init_tests() {
+    test_num = 2; 
+
+    tests[0].throttle =  10;
+    tests[0].brake = -5;
+    tests[0].vels_n = 0;
+    
+    tests[1].throttle =  20;
+    tests[1].brake = -5;
+    tests[1].vels_n = 0;
+}
+
 
 int main(int argc, char **argv) {
 
@@ -156,6 +238,8 @@ int main(int argc, char **argv) {
 
     ros::Subscriber ssub = n.subscribe("scan", 1, laser_recv);
     drive_pub = n.advertise<race::drive_param>("drive_parameters", 1);
+
+    init_tests();
 
     viz_init();
     while(ros::ok() && viz_update()) {
