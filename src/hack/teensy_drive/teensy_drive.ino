fa1bb420f13aa1639dcbba443ae6caef0d81851c
Francesco Gatti <gattifrancesco@hotmail.it>
2017-03-03 16:19:40 +0100

    fixed teensy_drive led on manual

---
+++ src/hack/teensy_drive/teensy_drive.ino
@@ -0,0 +1,131 @@
+#include <ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Empty.h>
+#include <race/drive_values.h>
+ros::NodeHandle  nh;
+
+
+
+boolean flagStop = false;
+int pwm_center_value = 9830;  //  15% duty cycle
+int pwm_lowerlimit = 6554;    //  10% duty cycle
+int pwm_upperlimit = 13108;   //  20% duty cycle
+
+std_msgs::Int32 str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+int kill_pin = 2;
+unsigned long duration = 0;
+
+boolean manual = false;
+
+void writepwm(int pwm_angle, int pwm_drive) 
+{
+  if (pwm_drive < pwm_lowerlimit) {
+    analogWrite(5, pwm_lowerlimit);   //  Safety lower limit
+  
+  } else if (pwm_drive > pwm_upperlimit) {
+    analogWrite(5, pwm_upperlimit);   //  Safety upper limit
+  
+  } else {
+    analogWrite(5, pwm_drive);    //  Incoming data
+  }
+
+  if (pwm_angle < pwm_lowerlimit) {
+    analogWrite(6, pwm_lowerlimit);   //  Safety lower limit
+  
+  } else if (pwm_angle > pwm_upperlimit) {
+    analogWrite(6, pwm_upperlimit);   //  Safety upper limit
+  
+  } else {
+    analogWrite(6, pwm_angle);    //  Incoming data
+  }
+}
+
+
+void messageDrive( const race::drive_values& pwm )
+{
+  if (manual)
+    return;
+  
+  if (flagStop == false) {
+    str_msg.data = pwm.pwm_drive;
+    chatter.publish( &str_msg );
+
+    writepwm(pwm.pwm_angle, pwm.pwm_drive);
+  
+  } else {
+    analogWrite(5, pwm_center_value);
+    analogWrite(6, pwm_center_value);
+  }
+}
+
+void messageEmergencyStop( const std_msgs::Bool& flag )
+{
+  flagStop = flag.data;
+  if (flagStop == true) {
+    analogWrite(5, pwm_center_value);
+    analogWrite(6, pwm_center_value);
+  } else {
+    manual = false;
+    str_msg.data = manual + 1000;
+    chatter.publish( &str_msg );
+  }
+}
+
+
+ros::Subscriber<race::drive_values> sub_drive("drive_pwm", &messageDrive );
+ros::Subscriber<std_msgs::Bool> sub_stop("eStop", &messageEmergencyStop );
+
+void setup() {
+
+  analogWriteFrequency(5, 100);
+  analogWriteFrequency(6, 100);
+  analogWriteResolution(16);
+
+  pinMode(14, INPUT);
+  pinMode(15, INPUT);
+  
+  analogWrite(5, pwm_center_value);
+  analogWrite(6, pwm_center_value);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+
+  nh.initNode();
+  nh.subscribe(sub_drive);
+  nh.subscribe(sub_stop);
+
+  nh.advertise(chatter);
+  //nh.advertise(pub);
+
+}
+
+void loop() {
+  unsigned long pwm_angle = pulseIn(15, HIGH, 30000);
+  unsigned long pwm_drive = pulseIn(14, HIGH, 30000);
+
+  str_msg.data = manual;
+  chatter.publish( &str_msg );
+  
+  if ( (pwm_angle > 500 && pwm_angle < 2500) && (pwm_angle > 1550 || pwm_drive > 1550 || pwm_angle < 1450 || pwm_drive < 1450)) {
+    manual = true;
+  }
+  if(manual) {
+    int angle = map(pwm_angle, 1000, 2000, pwm_lowerlimit, pwm_upperlimit);
+    int drive = map(pwm_drive, 1000, 2000, pwm_lowerlimit, pwm_upperlimit);
+    writepwm(angle, drive);
+  }
+  
+  nh.spinOnce();
+
+  if(manual)
+    digitalWrite(2, LOW);
+  else
+    digitalWrite(2, HIGH);
+}
+
+
