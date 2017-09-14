b954f0e650d5c424ed884c6e430f1083ea317991
Francesco Gatti <gattifrancesco@hotmail.it>
2017-09-15 00:19:01 +0200

    tf pub for car

--- src/ackermann_vehicle/ackermann_vehicle_gazebo/scripts/drive2gazebo.py
+++ src/ackermann_vehicle/ackermann_vehicle_gazebo/scripts/drive2gazebo.py
@@ -3,13 +3,13 @@
 import rospy
 from race.msg import drive_param
 from ackermann_msgs.msg import AckermannDrive
-
+from gazebo_msgs.msg import ModelStates
+import tf
 '''
 this node get the drive parameters from the keyboard node and convert it in
 AckermannDrive for the simulator
 
 '''
-
 pub = rospy.Publisher('ackermann_cmd', AckermannDrive, queue_size=1)
 
 def control(data):
@@ -33,7 +33,16 @@ def control(data):
         pub.publish(msg)
         
 
+def tf_pub(data):
+        if "ackermann_vehicle" in data.name: 
+                pose = data.pose[1]
+                br = tf.TransformBroadcaster()
+                p = (pose.position.x, pose.position.y, pose.position.z)
+                o = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+                br.sendTransform(p, o, rospy.Time.now(), "footprint", "map")		
+
 if __name__ == '__main__':
 	rospy.init_node('sim_controller')
 	rospy.Subscriber("drive_parameters", drive_param, control)
+	rospy.Subscriber("/gazebo/model_states", ModelStates, tf_pub)
 	rospy.spin()
